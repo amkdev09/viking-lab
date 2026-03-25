5c8855b4d7ec67d13c2de088bde06af13719ac95
BossBT12 <timothyprecy@gmail.com>
2026-03-25 13:04:50 +0530

    fixes

--- src/components/sections/header.jsx
+++ src/components/sections/header.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const SectionHeader = ({ type, title, description, textAlign = 'center', darkMode = false }) => {
     return (
         <div className={`${textAlign === 'center' ? 'mx-auto' : ''} max-w-[760px] text-${textAlign}`}>
-            <span className={`inline-flex h-[24px] items-center rounded-[4px] px-3 text-[length:var(--f-fs-font-fs16)] font-medium ${darkMode ? 'bg-[#212936] text-[#fff]' : 'bg-[#E9EBEA] text-[#2d3542]'}`}>
+            <span className={`inline-flex items-center rounded-[4px] p-[6px_12px] mb-1 text-base font-medium ${darkMode ? 'bg-[#212936] text-[#fff]' : 'bg-[#E9EBEA] text-[#000]'}`}>
                 {type}
             </span>
-            <h2 className={`mt-4 font-['Poppins'] text-[length:var(--f-fs-font-fs48)] leading-[1.08] font-semibold tracking-[-0.02em] ${darkMode ?  'text-[#fff]' : 'text-[#111827]'}`}>
+            <h2 className={`mt-4 text-5xl leading-none font-semibold tracking-[-0.02em] ${darkMode ? 'text-[#fff]' : 'text-[#111827]'}`}>
                 {title}
             </h2>
-            <p className={`mt-4 max-w-[700px] text-[length:var(--f-fs-font-fs18)] leading-[1.55] ${darkMode ? 'text-[#fff]' : 'text-[#5f666f]'}`}>
+            <p className={`mt-4 max-w-[700px] text-lg leading-[1.55] ${darkMode ? 'text-[#fff]' : 'text-[#5f666f]'}`}>
                 {description}
             </p>
         </div>
